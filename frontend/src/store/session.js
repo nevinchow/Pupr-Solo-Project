7da1bde20ab6bc3d86f6c69ad17ce048fe8d6878
Nevin Chow <nevinchow@gmail.com>
2021-10-08 13:13:04 -0700

    home login and signup working

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,7 @@ export const login = (user) => async (dispatch) => {
   return response;
 };
 
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
@@ -48,12 +49,35 @@ const sessionReducer = (state = initialState, action) => {
   }
 };
 
-
 export const restoreUser = () => async dispatch => {
-  const response = await csrfFetch('/api/session');
-  const data = await response.json();
-  dispatch(setUser(data.user));
-  return response;
-};
+    const response = await csrfFetch('/api/session');
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+  export const signup = (user) => async (dispatch) => {
+    const { username, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+  };
+
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 
 export default sessionReducer;
